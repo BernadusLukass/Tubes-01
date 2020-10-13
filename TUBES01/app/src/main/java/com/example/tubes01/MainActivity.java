866dac5f5fb00dfe550dea80d41a1a4aad78093e
GGLuks <64063689+GGLuks@users.noreply.github.com>
2020-10-13 14:59:00 +0700

    test2

--- TUBES01/app/src/main/java/com/example/tubes01/MainActivity.java
+++ TUBES01/app/src/main/java/com/example/tubes01/MainActivity.java
@@ -3,14 +3,13 @@ package com.example.tubes01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.sax.TextElementListener;
 
 public class MainActivity extends AppCompatActivity {
-
+TextElementListener listener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //cek
-
     }
 }
